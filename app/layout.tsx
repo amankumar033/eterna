547ea91802db41e5d380ef3001ef6d11b244190c
amankumar033 <amankumar51462@gmail.com>
2026-01-22 16:45:07 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/lib/providers/ReduxProvider";
-roviders/QueryProvider";
+import QueryProvider from "@/lib/providers/QueryProvider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 const geistSans = Geist({
