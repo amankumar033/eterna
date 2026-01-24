31699210435e39a24622d703ce48438ae165e3af
amankumar033 <amankumar51462@gmail.com>
2026-01-24 19:13:36 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/lib/providers/ReduxProvider";
 import QueryProvider from "@/lib/providers/QueryProvider";
-import { TooltipProvider } from "@/components/ui/tooltip";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+import { 
   display: "swap",
   preload: true,
 });
